0baa051ed4b3aea728742a00346b99c31c8fb60c
Gulnaz Gimaletdinova <gulnaz1024@yandex.ru>
2024-11-30 19:42:40 +0600

    Add a button to play the video at half of its size or full size

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import cv2
 import numpy as np
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFileDialog, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QFileDialog, QLabel
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import QTimer
 
@@ -13,17 +13,26 @@ class VideoPlayerApp(QWidget):
         self.setWindowTitle('OpenCV Video Player')
         self.setGeometry(100, 100, 800, 600)
 
-        # Layout
-        layout = QVBoxLayout()
-
-        # Video display label
-        self.video_label = QLabel(self)
-        layout.addWidget(self.video_label)
+        # Layout for the buttons at the top
+        top_layout = QHBoxLayout()
 
         # Play button
         self.play_button = QPushButton('Open Video')
         self.play_button.clicked.connect(self.open_video)
-        layout.addWidget(self.play_button)
+        top_layout.addWidget(self.play_button)
+
+        # Toggle size button
+        self.toggle_size_button = QPushButton('Toggle Size (Half Size)')
+        self.toggle_size_button.clicked.connect(self.toggle_size)
+        top_layout.addWidget(self.toggle_size_button)
+
+        # Main layout
+        layout = QVBoxLayout()
+        layout.addLayout(top_layout)
+
+        # Video display label
+        self.video_label = QLabel(self)
+        layout.addWidget(self.video_label)
 
         self.setLayout(layout)
 
@@ -33,6 +42,7 @@ class VideoPlayerApp(QWidget):
 
         self.cap = None
         self.current_frame = None
+        self.is_half_size = False  # Track whether the video is in half size or full size
 
     def open_video(self):
         # Open file dialog to select a video
@@ -59,6 +69,10 @@ class VideoPlayerApp(QWidget):
                 # Convert frame to RGB (OpenCV uses BGR)
                 rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+                # Resize frame if in half size mode
+                if self.is_half_size:
+                    rgb_frame = cv2.resize(rgb_frame, (rgb_frame.shape[1] // 2, rgb_frame.shape[0] // 2))
+
                 # Convert to QImage for displaying in PyQt
                 h, w, ch = rgb_frame.shape
                 bytes_per_line = ch * w
@@ -66,9 +80,21 @@ class VideoPlayerApp(QWidget):
 
                 # Set the QImage as a pixmap on the label
                 self.video_label.setPixmap(QPixmap.fromImage(qimg))
+
+                # Resize the window based on video size
+                self.resize(w, h)
             else:
                 self.cap.release()
                 self.timer.stop()  # Stop the timer when video ends
+
+    def toggle_size(self):
+        # Toggle the size of the video between full size and half size
+        self.is_half_size = not self.is_half_size
+
+        if self.is_half_size:
+            self.toggle_size_button.setText('Toggle Size (Full Size)')
+        else:
+            self.toggle_size_button.setText('Toggle Size (Half Size)')
 
 
 if __name__ == "__main__":
